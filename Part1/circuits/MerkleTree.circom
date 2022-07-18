fc1d4f6ce6960ef1a36a1b51abc81b19e7f4c2e1
RogerTaule <roger.taulee@gmail.com>
2022-07-19 00:13:57 +0200

    Week 2 assignment completed

--- Part1/circuits/MerkleTree.circom
+++ Part1/circuits/MerkleTree.circom
@@ -7,7 +7,26 @@ template CheckRoot(n) { // compute the root of a MerkleTree of n Levels
     signal output root;
 
     //[assignment] insert your code here to calculate the Merkle root from 2^n leaves
+    component poseidon[2**n - 1];
+
+    var h = 0;
+
+    for(var i = n; i > 0; i--) {
+        for(var j = 0; j < 2**(i - 1); j++) {
+            poseidon[h] = Poseidon(2);
+            if(i == n) {
+                poseidon[h].inputs[0] <== leaves[2*j];
+                poseidon[h].inputs[1] <== leaves[2*j + 1];
+            } else {
+                poseidon[h].inputs[0] <== poseidon[h - 2**i].out;
+                poseidon[h].inputs[1] <== poseidon[h - 2**i + 1].out;
+            }
+            h += 1;
+        }         
+    }
+    root <== poseidon[2**n - 2].out;
 }
+
 
 template MerkleTreeInclusionProof(n) {
     signal input leaf;
@@ -16,4 +35,17 @@ template MerkleTreeInclusionProof(n) {
     signal output root; // note that this is an OUTPUT signal
 
     //[assignment] insert your code here to compute the root from a leaf and elements along the path
+
+    component poseidon[n];
+    
+    for(var i = 0; i < n; i++) {
+        poseidon[i] = Poseidon(2);
+       
+        var current = i == 0 ? leaf : poseidon[i - 1].out;
+
+        poseidon[i].inputs[0] <== current + path_index[i]*(path_elements[i] - current);
+        poseidon[i].inputs[1] <== path_elements[i] + path_index[i]*(current - path_elements[i]);
+    }
+
+    root <== poseidon[n - 1].out;   
 }
--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -1,7 +1,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import { PoseidonT3 } from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
+import {PoseidonT3} from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
+
 import "./verifier.sol"; //inherits with the MerkleTreeInclusionProof verifier contract
 
 contract MerkleTree is Verifier {
@@ -9,21 +10,78 @@ contract MerkleTree is Verifier {
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
 
+    uint256 constant n = 3;
+
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+        for (uint256 i = 0; i < 2**n; ++i) {
+            hashes.push(0);
+        }
+
+        uint256 init = 0;
+
+        //Initialize everything else
+        for (uint256 i = 3; i > 0; --i) {
+            for (uint256 j = 0; j < 2**i; j += 2) {
+                uint256 hashedValue = PoseidonT3.poseidon(
+                    [hashes[init + j], hashes[init + j + 1]]
+                );
+                hashes.push(hashedValue);
+            }
+
+            init += 2**i;
+        }
+
+        root = hashes[2**(n + 1) - 2];
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
         // [assignment] insert a hashed leaf into the Merkle tree
+
+        require(index < 2**n, "All leaves are fulfilled");
+        hashes[index] = hashedLeaf;
+
+        uint256 pos = index;
+        index += 1;
+
+        uint256 init = 0;
+        uint256 hashedValue = 0;
+
+        for (uint256 i = n; i > 0; --i) {
+            if ((pos + 1) % 2 == 0) {
+                hashedValue = PoseidonT3.poseidon(
+                    [hashes[pos - 1], hashes[pos]]
+                );
+            } else {
+                hashedValue = PoseidonT3.poseidon(
+                    [hashes[pos], hashes[pos + 1]]
+                );
+            }
+
+            pos = init + 2**i + ((pos - init) / 2);
+            hashes[pos] = hashedValue;
+
+            init += 2**i;
+        }
+
+        root = hashes[2**(n + 1) - 2];
+
+        return root;
     }
 
     function verify(
-            uint[2] memory a,
-            uint[2][2] memory b,
-            uint[2] memory c,
-            uint[1] memory input
-        ) public view returns (bool) {
-
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[1] memory input
+    ) public view returns (bool) {
         // [assignment] verify an inclusion proof and check that the proof root matches current root
+        bool verifyInclusion = verifyProof(a, b, c, input);
+        bool verifyRoot = input[0] == root;
+        return verifyRoot && verifyInclusion;
+    }
+
+    function tryPoseidon() public pure returns (uint256) {
+        return PoseidonT3.poseidon([uint256(4), uint256(5)]);
     }
 }
